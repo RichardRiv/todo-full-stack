2bcf4469a667d272237c25f23bb239a8d289d12a
RichardRiv <r.richardev@gmail.com>
2026-07-31 11:14:09 -0400

    Changed default port

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import connectDB from './db.js';
 import todoRoutes from './routes/todo.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // {origin: 'http://localhost:5173'}
 app.use(cors());
